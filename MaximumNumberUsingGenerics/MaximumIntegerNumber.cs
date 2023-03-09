26ead91debcd718b184fb57b297bed1c6755dcc2
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-03-09 09:36:10 +0530

    Add: UC1-FindMaximumIntergerNumber

---
+++ MaximumNumberUsingGenerics/MaximumIntegerNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumNumberUsingGenerics
+{
+    internal class MaximumIntegerNumber
+    {
+        public static int MaximumIntegerCheck(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+            {
+                return firstNumber;
+            }
+            if(secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber)>0)
+            {
+                return secondNumber;
+            }
+            if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+            {
+                return thirdNumber;
+            }
+            throw new Exception("firstNumber, secondNumber, thirdNumber are same");
+        }
+
+    }
+}
---
+++ MaximumNumberUsingGenerics/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MaximumNumberUsingGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Maximum Integer Number:");
+            int output = MaximumIntegerNumber.MaximumIntegerCheck(11, 22, 33);
+            Console.WriteLine(output);
+        }
+    }
+}
