6daac4607ca2aa9c14ca6b3d7b59c3530f6d5f2e
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-03-09 23:51:07 +0530

    Add: UC4-ExtendMaxMethod

--- MaximumNumberUsingGenerics/MaximumIntegerNumber.cs
+++ MaximumNumberUsingGenerics/MaximumIntegerNumber.cs
@@ -6,56 +6,76 @@ using System.Threading.Tasks;
 
 namespace MaximumNumberUsingGenerics
 {
-    internal class MaximumIntegerNumber
+    public class GenericMaximum<T> where T : IComparable
     {
-        public static int MaximumIntegerCheck(int firstNumber, int secondNumber, int thirdNumber)
+        public T firstNumber, secondNumber, thirdNumber;
+        public GenericMaximum(T firstNumber, T secondNumber, T thirdNumber)
         {
-            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
-            {
-                return firstNumber;
-            }
-            if(secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber)>0)
-            {
-                return secondNumber;
-            }
-            if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
-            {
-                return thirdNumber;
-            }
-            throw new Exception("firstNumber, secondNumber, thirdNumber are same");
-        }
-        public static double MaximumFloatCheck(double firstNumber, double secondNumber, double thirdNumber)
-        {
-            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
-            {
-                return firstNumber;
-            }
-            if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
-            {
-                return secondNumber;
-            }
-            if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
-            {
-                return thirdNumber;
-            }
-            throw new Exception("firstNumber, secondNumber, thirdNumber are same");
+            this.firstNumber = firstNumber;
+            this.secondNumber = secondNumber;
+            this.thirdNumber = thirdNumber;
         }
 
-        public static string MaximumStringCheck(string firstNumber, string secondNumber, string thirdNumber)
+        public static T MaxNumber(T firstNumber, T secondNumber, T thirdNumber)
         {
-            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0 ||
+                firstNumber.CompareTo(secondNumber) >= 0 && firstNumber.CompareTo(thirdNumber) > 0 ||
+                firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo((T)firstNumber) >= 0)
             {
                 return firstNumber;
             }
-            if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+            if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0 ||
+                secondNumber.CompareTo(firstNumber) >= 0 && secondNumber.CompareTo(thirdNumber) > 0 ||
+                secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) >= 0)
             {
                 return secondNumber;
             }
-            if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+            if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0 ||
+                thirdNumber.CompareTo(firstNumber) >= 0 && thirdNumber.CompareTo(secondNumber) > 0 ||
+                thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) >= 0)
+
             {
                 return thirdNumber;
             }
-            throw new Exception("firstNumber, secondNumber, thirdNumber are same");
+            return default;
+        }
+        //public static double MaximumFloatCheck(double firstNumber, double secondNumber, double thirdNumber)
+        //{
+        //    if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+        //    {
+        //        return firstNumber;
+        //    }
+        //    if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+        //    {
+        //        return secondNumber;
+        //    }
+        //    if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+        //    {
+        //        return thirdNumber;
+        //    }
+        //    throw new Exception("firstNumber, secondNumber, thirdNumber are same");
+        //}
+
+        //public static string MaximumStringCheck(string firstNumber, string secondNumber, string thirdNumber)
+        //{
+        //    if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+        //    {
+        //        return firstNumber;
+        //    }
+        //    if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+        //    {
+        //        return secondNumber;
+        //    }
+        //    if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+        //    {
+        //        return thirdNumber;
+        //    }
+        //    throw new Exception("firstNumber, secondNumber, thirdNumber are same");
+        //}
+        public T MaxMethod()
+        {
+            T max = GenericMaximum<T>.MaxNumber(this.firstNumber, this.secondNumber, this.thirdNumber);
+            return max;
         }
     }
 }
