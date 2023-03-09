b2c7fbd968fb371a19532f831f727ecb353e9da0
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-03-09 10:00:08 +0530

    Add: UC3-MaximumStringNumber

--- MaximumNumberUsingGenerics/MaximumIntegerNumber.cs
+++ MaximumNumberUsingGenerics/MaximumIntegerNumber.cs
@@ -41,5 +41,21 @@ namespace MaximumNumberUsingGenerics
             throw new Exception("firstNumber, secondNumber, thirdNumber are same");
         }
 
+        public static string MaximumStringCheck(string firstNumber, string secondNumber, string thirdNumber)
+        {
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+            {
+                return firstNumber;
+            }
+            if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+            {
+                return secondNumber;
+            }
+            if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+            {
+                return thirdNumber;
+            }
+            throw new Exception("firstNumber, secondNumber, thirdNumber are same");
+        }
     }
 }
--- MaximumNumberUsingGenerics/Program.cs
+++ MaximumNumberUsingGenerics/Program.cs
@@ -7,9 +7,14 @@
             Console.WriteLine("Maximum Integer Number:");
             int output = MaximumIntegerNumber.MaximumIntegerCheck(11, 22, 33);
             Console.WriteLine(output);
+
             Console.WriteLine("Maximum Float Number:");
             double Doubleoutput = MaximumIntegerNumber.MaximumFloatCheck(11.11, 22.33, 33.44);
             Console.WriteLine(Doubleoutput);
+
+            Console.WriteLine("Maximum String Number:");
+            string Stringoutput = MaximumIntegerNumber.MaximumStringCheck("Apple","Peach","Banana");
+            Console.WriteLine(Stringoutput);
         }
     }
 }
